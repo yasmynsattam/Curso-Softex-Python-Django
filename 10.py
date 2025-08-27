7464cab23bc5c023713971e4f3ec0389d5731a26
yasmyn <224887934+yasmynsattam@users.noreply.github.com>
2025-08-27 11:41:56 -0300

    trocar palavra

---
+++ 10.py
@@ -0,0 +1,6 @@
+frase = input("Digite uma frase:")
+bomba = "bomba"
+if bomba in frase:
+    print(f"A palavra {bomba} aparece na frase.")
+else:
+    print(f"A palavra {bomba} não aparece na frase.") 
